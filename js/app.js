15e44d5adb0d6d0d326ba9bf0e5bf4f88022d9f1
Paul Cullen Rowe <prowe214@gmail.com>
2015-08-18 15:48:54 -0600

    added url query string functionality

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('calculator', ['ngRoute']);
 
 
 app.controller('CalculatorController', function ($scope, $routeParams) {
-  $scope.message = "ALIVE";
+  $scope.message = "ORIGINAL CALC";
   $scope.answer = function () {
     if ($routeParams.action === 'add') {
       return parseInt($routeParams.a) + parseInt($routeParams.b);
@@ -13,7 +13,21 @@ app.controller('CalculatorController', function ($scope, $routeParams) {
   };
 });
 
-app.config(function ($routeProvider) {
+app.controller('AdvancedController', function ($scope, $routeParams, $location) {
+  $scope.message = $routeParams.action;
+  $scope.answer = function () {
+    var query = $location.search();
+    if ($routeParams.action === 'add') {
+      return parseInt($location.search().x) + parseInt($location.search().y);
+    }
+    if ($routeParams.action === 'divide') {
+      return parseInt($location.search().x) / parseInt($location.search().y);
+    }
+  };
+});
+
+app.config(function ($routeProvider, $locationProvider) {
+      $locationProvider.html5Mode(true);
   $routeProvider
     .when('/', {
       templateUrl: 'partials/domath.html',
@@ -22,5 +36,9 @@ app.config(function ($routeProvider) {
     .when('/:action/:a/:b', {
       templateUrl: 'partials/domath.html',
       controller: 'CalculatorController'
+    })
+    .when('/:action/?', {
+      templateUrl: 'partials/domath.html',
+      controller: 'AdvancedController'
     });
 });
